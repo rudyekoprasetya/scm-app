92c28c098b7e7b2ac35e4c385b76a82b7a2b0e9e
Rudy Eko Prasetya <rudyekoprasetya@yahoo.co.id>
2026-05-28 11:14:05 +0700

    [EDIT] titlebar

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'SCM') }} - Supply Chain Management</title>
+        <title>{{ config('app.name', 'SCM') }} - Supply Chain Management Apps</title>
 
         <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
 
